7b628220fc78df99c42f0a9633d3d3ab0fb2d478
nckrtl <nick.retel@gmail.com>
2025-06-05 19:13:16 +0200

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use NckRtl\RouteMaker\Route;
+use NckRtl\RouteMaker\Get;
 
 class HomeController extends Controller
 {
-    #[Route(uri: '/')]
+    #[Get(uri: '/')]
     public function show(): \Inertia\ResponseFactory|\Inertia\Response
     {
         return inertia('Home');
